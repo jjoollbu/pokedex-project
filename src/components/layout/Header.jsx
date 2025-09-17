e5baeead8897213212ca09aacc512f4ac55ccf3a
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-09-17 16:29:08 -0300

    fix: fix github link

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,11 +8,7 @@ const Header = () => {
         <header className={styles.header}>
             <img src="" alt="" />
             <img src={pokemonLogo} alt="Pokemon Logo" className={styles.logo} />
-            <a
-                href="https://github.com/jjoollbu/pokedex-project/tree/develop"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
+            <a href="https://github.com/jjoollbu/pokedex-project" target="_blank" rel="noopener noreferrer">
                 <img src={githubLogo} alt="GitHub" className={styles.logoSmall} />
             </a>
         </header>
