abc3d179dcdcaa887971d716fb4713cb637b1447
Jonmar Neto <jonmarlimaneto@gmail.com>
2025-09-17 16:20:01 -0300

    feat: criação do header e estilização, faltando o switch de tema

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Header.module.css';
+import pokemonLogo from '../../assets/images/international_Pokémon_logo.svg.png';
+import githubLogo from '../../assets/images/github.png';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <img src="" alt="" />
+            <img src={pokemonLogo} alt="Pokemon Logo" className={styles.logo} />
+            <a
+                href="https://github.com/jjoollbu/pokedex-project/tree/develop"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src={githubLogo} alt="GitHub" className={styles.logoSmall} />
+            </a>
+        </header>
+    );
+};
+
+export default Header;
