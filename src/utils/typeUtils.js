f07ef7ec5f8fef42cac1bf13ecc9da2cd92c2542
Willian Figueiredo <willianfigueiredodev@gmail.com>
2025-09-19 13:31:52 -0300

    Mudança de cor nos cards

--- src/utils/typeUtils.js
+++ src/utils/typeUtils.js
@@ -41,9 +41,9 @@ export const typeIcons = {
 };
 
 export const typeColors = {
-  normal: '#A8A77A', fire: '#EE8130', water: '#6390F0', electric: '#F7D02C',
-  grass: '#7AC74C', ice: '#96D9D6', fighting: '#C22E28', poison: '#A33EA1',
-  ground: '#E2BF65', flying: '#A98FF3', psychic: '#F95587', bug: '#A6B91A',
-  rock: '#B6A136', ghost: '#735797', dragon: '#6F35FC', dark: '#705746',
-  steel: '#B7B7CE', fairy: '#D685AD',
+  normal: '#A8A77A', fire: '#f8720cff', water: '#2065faff', electric: '#ebc42aff',
+  grass: '#6aec1eff', ice: '#aaf3efff', fighting: '#ff3730ff', poison: '#8400ffff',
+  ground: '#E2BF65', flying: '#a686fdff', psychic: '#f16d95ff', bug: '#c6df0fff',
+  rock: '#B6A136', ghost: '#735797', dragon: '#682ff8ff', dark: '#705746',
+  steel: '#cccce6ff', fairy: '#D685AD',
 };
