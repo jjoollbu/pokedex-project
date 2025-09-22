9f4590faf26e66fb969ba1fe8b39be985baab2f9
Willian Figueiredo <willianfigueiredodev@gmail.com>
2025-09-22 14:48:57 -0300

    Add cores de Icons:light e Card:strong

--- src/utils/typeUtils.js
+++ src/utils/typeUtils.js
@@ -1,49 +1,48 @@
-// src/utils/typeUtils.js
-
-import bugIcon from '../assets/images/icons/bug.png';
-import darkIcon from '../assets/images/icons/dark.png';
-import dragonIcon from '../assets/images/icons/dragon.png';
-import electricIcon from '../assets/images/icons/electric.png';
-import fairyIcon from '../assets/images/icons/fairy.png';
-import fightingIcon from '../assets/images/icons/fighting.png';
-import fireIcon from '../assets/images/icons/fire.png';
-import flyingIcon from '../assets/images/icons/flying.png';
-import ghostIcon from '../assets/images/icons/ghost.png';
-import grassIcon from '../assets/images/icons/grass.png';
-import groundIcon from '../assets/images/icons/ground.png';
-import iceIcon from '../assets/images/icons/ice.png';
-import normalIcon from '../assets/images/icons/normal.png';
-import poisonIcon from '../assets/images/icons/poison.png';
-import psychicIcon from '../assets/images/icons/psychic.png';
-import rockIcon from '../assets/images/icons/rock.png';
-import steelIcon from '../assets/images/icons/steel.png';
-import waterIcon from '../assets/images/icons/water.png';
+import bugIcon from "../assets/images/icons/bug.png";
+import darkIcon from "../assets/images/icons/dark.png";
+import dragonIcon from "../assets/images/icons/dragon.png";
+import electricIcon from "../assets/images/icons/electric.png";
+import fairyIcon from "../assets/images/icons/fairy.png";
+import fightingIcon from "../assets/images/icons/fighting.png";
+import fireIcon from "../assets/images/icons/fire.png";
+import flyingIcon from "../assets/images/icons/flying.png";
+import ghostIcon from "../assets/images/icons/ghost.png";
+import grassIcon from "../assets/images/icons/grass.png";
+import groundIcon from "../assets/images/icons/ground.png";
+import iceIcon from "../assets/images/icons/ice.png";
+import normalIcon from "../assets/images/icons/normal.png";
+import poisonIcon from "../assets/images/icons/poison.png";
+import psychicIcon from "../assets/images/icons/psychic.png";
+import rockIcon from "../assets/images/icons/rock.png";
+import steelIcon from "../assets/images/icons/steel.png";
+import waterIcon from "../assets/images/icons/water.png";
 
 export const typeIcons = {
-  bug: bugIcon,
-  dark: darkIcon,
-  dragon: dragonIcon,
-  electric: electricIcon,
-  fairy: fairyIcon,
-  fighting: fightingIcon,
-  fire: fireIcon,
-  flying: flyingIcon,
-  ghost: ghostIcon,
-  grass: grassIcon,
-  ground: groundIcon,
-  ice: iceIcon,
-  normal: normalIcon,
-  poison: poisonIcon,
-  psychic: psychicIcon,
-  rock: rockIcon,
-  steel: steelIcon,
-  water: waterIcon,
+  bug: bugIcon, dark: darkIcon, dragon: dragonIcon, electric: electricIcon,
+  fairy: fairyIcon, fighting: fightingIcon, fire: fireIcon, flying: flyingIcon,
+  ghost: ghostIcon, grass: grassIcon, ground: groundIcon, ice: iceIcon,
+  normal: normalIcon, poison: poisonIcon, psychic: psychicIcon, rock: rockIcon,
+  steel: steelIcon, water: waterIcon,
 };
 
 export const typeColors = {
-  normal: '#A8A77A', fire: '#f8720cff', water: '#2065faff', electric: '#ebc42aff',
-  grass: '#6aec1eff', ice: '#aaf3efff', fighting: '#ff3730ff', poison: '#8400ffff',
-  ground: '#E2BF65', flying: '#a686fdff', psychic: '#f16d95ff', bug: '#c6df0fff',
-  rock: '#B6A136', ghost: '#735797', dragon: '#682ff8ff', dark: '#705746',
-  steel: '#cccce6ff', fairy: '#D685AD',
+  normal:   { strong: '#A8A77A', light: '#DCDCDC' },
+  fire:     { strong: '#EE8130', light: '#FFB971' },
+  water:    { strong: '#6390F0', light: '#8CC4E2' },
+  electric: { strong: '#F7D02C', light: '#FFE662' },
+  grass:    { strong: '#7AC74C', light: '#A8FF98' },
+  ice:      { strong: '#96D9D6', light: '#8CF5E4' },
+  fighting: { strong: '#C22E28', light: '#DA7589' },
+  poison:   { strong: '#A33EA1', light: '#D6A2E4' },
+  ground:   { strong: '#E2BF65', light: '#E69A74' },
+  flying:   { strong: '#8caeecff', light: '#a1bbec' },
+  psychic:  { strong: '#F95587', light: '#FFA5DA' },
+  bug:      { strong: '#A6B91A', light: '#BAE05F' },
+  rock:     { strong: '#B6A136', light: '#C9BB8A' },
+  ghost:    { strong: '#735797', light: '#8291E0' },
+  dragon:   { strong: '#6F35FC', light: '#88A2E8' },
+  dark:     { strong: '#705746', light: '#8E8C94' },
+  steel:    { strong: '#B7B7CE', light: '#9FB8B9' },
+  fairy:    { strong: '#D685AD', light: '#FDB9E9' },
+  default:  { strong: '#A8A77A', light: '#DCDCDC' },
 };
